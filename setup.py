2845d5e693612e1fe3e18a3376f685912c8969ab
gwkoh <gwkoh@nexon.co.kr>
2019-03-13 00:18:27 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'lazyer',
+    version = '0.1.0',
+    description = '',
+    author = 'Gyeongwan Koh',
+    author_email = 'gwkoh@nexon.co.kr',
+    install_requires = [],
+    packages = find_packages('tests'),
+)
